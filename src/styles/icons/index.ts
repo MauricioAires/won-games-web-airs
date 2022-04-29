7d0c3abc1daac6d2982f9da73e122ffceb001203
Mauricio Aires de Freitas <mauricioaires015@gmail.com>
2022-04-29 12:04:21 -0300

    feat: add icons

---
+++ src/styles/icons/index.ts
@@ -0,0 +1,6 @@
+import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
+import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
+import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
+
+export { MenuIcon, ShoppingCartIcon, SearchIcon, CloseIcon }

d81f3f9bd4e9003f7785d8b382940eb57c8ff16d
Mauricio Aires de Freitas <mauricioaires015@gmail.com>
2022-06-04 12:40:43 -0300

    adicionar teste

--- src/components/BannnerSlider/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from './styles'
-
-const BannnerSlider = () => (
-  <S.Wrapper>
-    <h1>BannnerSlider</h1>
-  </S.Wrapper>
-)
-
-export default BannnerSlider
--- src/components/BannnerSlider/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react'
-import BannnerSlider from '.'
-
-export default {
-  title: 'BannnerSlider',
-  component: BannnerSlider
-} as Meta
-
-export const Default: Story = () => <BannnerSlider />

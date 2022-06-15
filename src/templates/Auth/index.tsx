0879e996cd33124d7064105f795eb732235328dd
Mauricio Aires de Freitas <mauricioaires015@gmail.com>
2022-06-15 01:12:44 -0300

    add link to home in sign in and sign up pages

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link'
 import Heading from 'components/Heading'
 import Logo from 'components/Logo'
+
 import * as S from './styles'
 
 export type AuthProps = {
@@ -11,7 +13,11 @@ const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
       <S.BannerContent>
-        <Logo id="banner" />
+        <Link href="/">
+          <a>
+            <Logo id="banner" />
+          </a>
+        </Link>
         <div>
           <Heading size="huge">All your favorite game in one place</Heading>
           <S.Subtitle>
@@ -23,7 +29,11 @@ const Auth = ({ title, children }: AuthProps) => (
     </S.BannerBlock>
     <S.Content>
       <S.ContentWrapper>
-        <Logo id="content" color="black" size="large" />
+        <Link href="/">
+          <a>
+            <Logo id="content" color="black" size="large" />
+          </a>
+        </Link>
         <Heading color="black" lineColor="secondary" lineLeft>
           {title}
         </Heading>

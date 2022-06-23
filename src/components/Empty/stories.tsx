0d10d6c97a5c00604e8fe3666e144e4294453abe
Mauricio Aires de Freitas <mauricioaires015@gmail.com>
2022-06-22 21:44:46 -0300

    add empty component

---
+++ src/components/Empty/stories.tsx
@@ -0,0 +1,21 @@
+import { Story, Meta } from '@storybook/react'
+import Empty, { EmptyProps } from '.'
+
+export default {
+  title: 'commom/Empty',
+  component: Empty,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+const Template: Story<EmptyProps> = (args) => <Empty {...args} />
+
+export const Default = Template.bind({})
+
+Default.args = {
+  title: 'Your wishlist is empty',
+  description: 'Games added to your wishlist will appear here'
+}
---
+++ src/components/Empty/test.tsx
@@ -0,0 +1,51 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Empty, { EmptyProps } from '.'
+
+const props: EmptyProps = {
+  title: 'A simple title',
+  description: 'A simples description',
+  hasLink: true
+}
+
+const sut = (props: EmptyProps) => renderWithTheme(<Empty {...props} />)
+
+describe('<Empty />', () => {
+  it('should render correctly', () => {
+    const { container } = sut(props)
+
+    expect(
+      screen.getByRole('image', {
+        name: /a gamer in a couch playing videogame/i
+      })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', {
+        name: /a simple title/i
+      })
+    ).toBeInTheDocument()
+    expect(screen.getByText(/a simples description/i)).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('link', {
+        name: /go back to store/i
+      })
+    ).toHaveAttribute('href', '/')
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should not render link when hasLink is not passed', () => {
+    sut({
+      ...props,
+      hasLink: false
+    })
+
+    expect(
+      screen.queryByRole('link', {
+        name: /go back to store/i
+      })
+    ).not.toBeInTheDocument()
+  })
+})

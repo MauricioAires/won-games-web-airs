2b2d6ea6950a90ebb3f724e32f181c6a477d2298
Mauricio Aires de Freitas <mauricioaires015@gmail.com>
2022-07-07 13:37:12 -0300

    adicionar filtro dinamico e utils

---
+++ src/utils/filter/index.ts
@@ -0,0 +1,51 @@
+import { ItemProps } from './../../components/ExploreSidebar/index'
+import { ParsedUrlQueryInput } from 'querystring'
+
+type ParceArgs = {
+  queryString: ParsedUrlQueryInput
+  filterItems: Pick<ItemProps, 'name' | 'type'>[]
+}
+
+export const parseQueryStringToWhere = ({
+  queryString,
+  filterItems
+}: ParceArgs) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const obj: any = {}
+
+  Object.keys(queryString)
+    .filter((item) => item !== 'sort')
+    .forEach((key) => {
+      const item = filterItems.find((item) => item.name === key)
+      const isCheckbox = item?.type === 'checkbox'
+      const filterValue = queryString[key]
+
+      obj[key] = !isCheckbox
+        ? filterValue
+        : {
+            name_contains: filterValue
+          }
+    })
+
+  return obj
+}
+
+export const parceQueryStringToFilter = ({
+  queryString,
+  filterItems
+}: ParceArgs) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const obj: any = {}
+
+  Object.keys(queryString).forEach((key) => {
+    const item = filterItems.find((item) => item.name === key)
+    const itemValue = queryString[key]
+    const isCheckbox = item?.type === 'checkbox'
+    const isArray = Array.isArray(itemValue)
+
+    // verificae se não é um array, se não, crisar
+    obj[key] = !isArray && isCheckbox ? [itemValue] : itemValue
+  })
+
+  return obj
+}

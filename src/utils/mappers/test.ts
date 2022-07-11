cfe1cdf8a81d16f1906a6bd9eff8647c4ac100a8
Mauricio Aires de Freitas <mauricioaires015@gmail.com>
2022-07-10 22:01:20 -0300

    adicionar teste para os mappers

---
+++ src/utils/mappers/test.ts
@@ -0,0 +1,109 @@
+import { QueryGames_games } from './../../graphql/generated/QueryGames'
+import {
+  QueryHome_banners,
+  QueryHome_sections_freeGames_highlight
+} from './../../graphql/generated/QueryHome'
+import { highlightMapper } from './../mappers'
+import { gamesMapper } from 'utils/mappers'
+import { bannerMapper } from '.'
+
+describe('$bannerMapper', () => {
+  it('should return the rigth format when mapper', () => {
+    const banner = [
+      {
+        __typename: 'Banner',
+        title: 'banner title',
+        subtitle: 'banner subtitle',
+        image: {
+          url: '/imgage.jpg'
+        },
+        button: {
+          label: 'button label',
+          link: 'button link'
+        },
+        ribbon: {
+          text: 'ribbon text',
+          color: 'primary',
+          size: 'small'
+        }
+      } as QueryHome_banners
+    ]
+
+    expect(bannerMapper(banner)).toStrictEqual([
+      {
+        img: '/imgage.jpg',
+        title: 'banner title',
+        subtitle: 'banner subtitle',
+        buttonLabel: 'button label',
+        buttonLink: 'button link',
+        ribbon: 'ribbon text',
+        ribbonColor: 'primary',
+        ribbonSize: 'small'
+      }
+    ])
+  })
+})
+describe('$gamesMapper', () => {
+  it('should return an empty array if there are no games', () => {
+    expect(gamesMapper(null)).toStrictEqual([])
+  })
+
+  it('should return the rigth format when mapper', () => {
+    const games = [
+      {
+        __typename: 'Game',
+        id: '1',
+        name: 'game title',
+        slug: 'game_slug',
+        cover: {
+          url: 'cover.jpg'
+        },
+        developers: [
+          {
+            name: 'developer name'
+          }
+        ],
+        price: 100
+      } as QueryGames_games
+    ]
+
+    expect(gamesMapper(games)).toStrictEqual([
+      {
+        id: '1',
+        title: 'game title',
+        slug: 'game_slug',
+        developer: 'developer name',
+        img: 'cover.jpg',
+        price: 100
+      }
+    ])
+  })
+})
+describe('$bannerMapper', () => {
+  it('should return the rigth format when mapper', () => {
+    const highlight = {
+      __typename: 'ComponentPageHighlight',
+      title: 'highlight title',
+      subtitle: 'highlight subtitle',
+      background: {
+        url: 'background.jpg'
+      },
+      floatImage: {
+        url: 'floatImage.jpg'
+      },
+      buttonLabel: 'button label',
+      buttonLink: 'button link',
+      alignment: 'left'
+    } as QueryHome_sections_freeGames_highlight
+
+    expect(highlightMapper(highlight)).toStrictEqual({
+      title: 'highlight title',
+      subtitle: 'highlight subtitle',
+      backgroundImage: 'background.jpg',
+      floatImage: 'floatImage.jpg',
+      buttonLabel: 'button label',
+      buttonLink: 'button link',
+      alignment: 'left'
+    })
+  })
+})
